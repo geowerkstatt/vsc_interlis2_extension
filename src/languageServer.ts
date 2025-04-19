d05b3ae4d7fb5518fe338170268a3195db93176c
Nenad Stevic <nenad.stevic@geowerkstatt.ch>
2025-04-19 16:58:56 +0200

    Extract start Language Server Logic

--- src/languageServer.ts
+++ src/languageServer.ts
@@ -1,3 +1,25 @@
-import { LanguageClient } from "vscode-languageclient/node";
+import { LanguageClient, LanguageClientOptions, ServerOptions } from "vscode-languageclient/node";
+import { cleanupTempDir, getServerExecutable } from "./utils";
+import * as vscode from "vscode";
 
 let client: LanguageClient;
+
+export async function startLanguageServer(context: vscode.ExtensionContext) {
+  cleanupTempDir();
+
+  const serverOptions: ServerOptions = {
+    run: getServerExecutable(context, false),
+    debug: getServerExecutable(context, true),
+  };
+
+  const clientOptions: LanguageClientOptions = {
+    documentSelector: [{ scheme: "file", language: "INTERLIS2" }],
+    synchronize: {
+      fileEvents: vscode.workspace.createFileSystemWatcher("**/*.ili"),
+    },
+  };
+
+  client = new LanguageClient("INTERLIS2LanguageServer", "INTERLIS2 Language Server", serverOptions, clientOptions);
+
+  await client.start();
+}
