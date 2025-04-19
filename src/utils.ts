b97be4f882d25eba61782d735ffb7e0c50e38601
Nenad Stevic <nenad.stevic@geowerkstatt.ch>
2025-04-19 16:53:25 +0200

    Extract and refactor getServerExecutable

--- src/utils.ts
+++ src/utils.ts
@@ -1,6 +1,8 @@
 import * as os from "node:os";
 import * as path from "node:path";
 import * as fs from "node:fs";
+import * as vscode from "vscode";
+import { Executable, TransportKind } from "vscode-languageclient/node";
 
 export const tempDir = path.join(os.tmpdir(), "InterlisLanguageSupport");
 
@@ -27,4 +29,15 @@ export function getRuntimeId(): string {
     default:
       throw new Error(`Unsupported platform: ${process.platform}`);
   }
+}
+
+export function getServerExecutable(context: vscode.ExtensionContext, debug: boolean): Executable {
+  const exeExt = process.platform === "win32" ? ".exe" : "";
+  const serverName = `Geowerkstatt.Interlis.LanguageServer${exeExt}`;
+  const runPath = `language-server/bin/${getRuntimeId()}/${serverName}`;
+  const debugPath = `language-server/src/Geowerkstatt.Interlis.LanguageServer/bin/Debug/net8.0/${serverName}`;
+  const absoluteDebug = context.asAbsolutePath(debugPath);
+
+  const command = debug && fs.existsSync(absoluteDebug) ? absoluteDebug : context.asAbsolutePath(runPath);
+  return { command: command, transport: TransportKind.stdio };
 }
