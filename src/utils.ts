0f933b106aad042a31c27005105a91e566921d66
Nenad Stevic <nenad.stevic@geowerkstatt.ch>
2025-04-19 16:47:14 +0200

    Create method for temp dir cleanup

--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,12 @@
 import * as os from "node:os";
 import * as path from "node:path";
+import * as fs from "node:fs";
 
 export const tempDir = path.join(os.tmpdir(), "InterlisLanguageSupport");
+
+export function cleanupTempDir() {
+  if (fs.existsSync(tempDir)) {
+    fs.rmSync(tempDir, { recursive: true, force: true });
+  }
+}
+
