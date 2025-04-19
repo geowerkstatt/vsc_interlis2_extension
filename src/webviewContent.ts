53c4b4b1a5dc18680059a316cc7cdfb34a2c8ee7
Nenad Stevic <nenad.stevic@geowerkstatt.ch>
2025-04-19 15:42:38 +0200

    Add method to configure raw html

---
+++ src/webviewContent.ts
@@ -0,0 +1,30 @@
+import * as vscode from "vscode";
+import * as fs from "fs";
+
+export function getWebviewHTML(webview: vscode.Webview, extensionURI: vscode.Uri): string {
+  const mermaidLibUriStr = "https://cdn.jsdelivr.net/npm/mermaid@10/dist/mermaid.esm.min.js";
+
+  const cspSource = webview.cspSource;
+  const contentSecurityPolicy = `
+    default-src 'none';
+    style-src ${cspSource} 'unsafe-inline';
+    img-src ${cspSource} data;
+    script-src ${webview.cspSource}
+    ${mermaidLibUriStr.startsWith("https://cdn.jsdelivr.net") ? "https://cdn.jsdelivr.net" : ""};
+    connect-src 'none';
+  `;
+
+  // --- Load HTML Template ---
+  const htmlFilePath = vscode.Uri.joinPath(extensionURI, "out", "webview", "/assets/webview.html");
+  let htmlContent = "";
+  try {
+    htmlContent = fs.readFileSync(htmlFilePath.fsPath, "utf8");
+  } catch (err) {
+    console.log(err);
+  }
+
+  htmlContent = htmlContent.replace(/${csp}/g, contentSecurityPolicy);
+  htmlContent = htmlContent.replace(/${mermaidUri}/g, mermaidLibUriStr);
+
+  return htmlContent;
+}
