987f01750a533a1eac530b81462a8add73b06f8f
Nenad Stevic <nenad.stevic@geowerkstatt.ch>
2025-05-02 11:20:08 +0200

    Fix bad comment

--- language-server/src/Geowerkstatt.Interlis.LanguageServer/Handlers/GenerateDiagramOptions.cs
+++ language-server/src/Geowerkstatt.Interlis.LanguageServer/Handlers/GenerateDiagramOptions.cs
@@ -1,7 +1,7 @@
 namespace Geowerkstatt.Interlis.LanguageServer.Handlers;
 
 /// <summary>
-/// Options for generating markdown documentation.
+/// Options for generating mermaid diagrams.
 /// </summary>
 /// <param name="Uri">The uri to identify the text document.</param>
 public record GenerateDiagramOptions(string? Uri);
