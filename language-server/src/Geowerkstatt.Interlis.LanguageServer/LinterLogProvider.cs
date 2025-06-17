a1eb6fdeb4f20e5ad2ef39dd5e2940feed623e30
tschumpr <roswita.tschuemperlin@geowerkstatt.ch>
2025-06-17 11:08:08 +0200

    Show compiler logs in linter

---
+++ language-server/src/Geowerkstatt.Interlis.LanguageServer/LinterLogProvider.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace Geowerkstatt.Interlis.LanguageServer;
+
+public class LinterLogProvider : ILoggerProvider
+{
+    private readonly LinterLogger logger = new LinterLogger();
+
+    public ILogger CreateLogger(string categoryName) => logger;
+    
+    public void Dispose()
+    {
+    }
+
+    public List<string> GetMessages() => logger.Messages;
+}
