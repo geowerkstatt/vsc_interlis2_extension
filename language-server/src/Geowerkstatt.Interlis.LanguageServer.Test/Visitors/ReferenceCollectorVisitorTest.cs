fdcb6f9ba09fc7df5668336e7f6e6e4e0dee79d5
Philipp Lüthi <philipp.luethi@geowerkstatt.ch>
2025-06-17 10:42:25 +0200

    Add test for IMPORTS references

--- language-server/src/Geowerkstatt.Interlis.LanguageServer.Test/Visitors/ReferenceCollectorVisitorTest.cs
+++ language-server/src/Geowerkstatt.Interlis.LanguageServer.Test/Visitors/ReferenceCollectorVisitorTest.cs
@@ -41,4 +41,44 @@ public class ReferenceCollectorVisitorTest
         Assert.AreEqual("ClassA", references[0].Target.Name);
         Assert.AreEqual("ClassB", references[1].Target.Name);
     }
+
+    private const string TestModelImports = """
+    INTERLIS 2.4;
+
+    MODEL TestModel (de) AT "http://models.geow.cloud" VERSION "1" =
+        IMPORTS GeometryCHLV95_V1;
+        TOPIC TestTopic =
+            CLASS ClassA =
+                attrA: TEXT*10;
+            END ClassA;
+
+            CLASS ClassB =
+                attrB: 10..20;
+            END ClassB;
+
+            ASSOCIATION Assoc1 =
+                AssocA -- {0..*} ClassA;
+                AssocB -<> {1} ClassB;
+            END Assoc1;
+        END TestTopic;
+    END TestModel.
+    """;
+
+    [TestMethod]
+    public void TestInterlisImports()
+    {
+        var reader = new InterlisReader();
+        var interlisFile = reader.ReadFile(new StringReader(TestModelImports));
+
+        var visitor = new ReferenceCollectorVisitor();
+        var references = visitor.VisitInterlisEnvironment(interlisFile);
+
+        Assert.IsNotNull(references);
+        Assert.AreEqual(3, references.Count);
+        Assert.AreEqual("GeometryCHLV95_V1", references[0].Target.Name);
+        Assert.AreEqual("ClassA", references[1].Target.Name);
+        Assert.AreEqual("ClassB", references[2].Target.Name);
+    }
+
+
 }
