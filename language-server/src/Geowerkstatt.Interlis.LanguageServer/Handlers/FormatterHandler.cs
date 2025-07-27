ac7e9dff38bb9fe7c3b62626e1d91c4e24f32da3
Patrick Ackermann <patrick.ackermann@geowerkstatt.ch>
2025-07-27 22:45:20 +0200

    Add Format Document

---
+++ language-server/src/Geowerkstatt.Interlis.LanguageServer/Handlers/FormatterHandler.cs
@@ -0,0 +1,58 @@
+using Geowerkstatt.Interlis.Compiler.CreateAST;
+using Geowerkstatt.Interlis.Compiler;
+using Geowerkstatt.Interlis.LanguageServer.Cache;
+using OmniSharp.Extensions.LanguageServer.Protocol.Client.Capabilities;
+using OmniSharp.Extensions.LanguageServer.Protocol.Document;
+using OmniSharp.Extensions.LanguageServer.Protocol.Models;
+using Microsoft.Extensions.Logging.Abstractions;
+using Position = OmniSharp.Extensions.LanguageServer.Protocol.Models.Position;
+using Range = OmniSharp.Extensions.LanguageServer.Protocol.Models.Range;
+
+namespace Geowerkstatt.Interlis.LanguageServer.Handlers;
+
+public class FormatterHandler : DocumentFormattingHandlerBase
+{
+    private readonly FileContentCache fileContentCache;
+
+    public FormatterHandler(FileContentCache fileContentCache)
+    {
+        this.fileContentCache = fileContentCache;
+    }
+
+    protected override DocumentFormattingRegistrationOptions CreateRegistrationOptions(DocumentFormattingCapability capability, ClientCapabilities clientCapabilities)
+    {
+        return new DocumentFormattingRegistrationOptions
+        {
+            DocumentSelector = TextDocumentSelector.ForPattern("**/*.ili")
+        };
+    }
+
+    public override async Task<TextEditContainer?> Handle(DocumentFormattingParams request, CancellationToken cancellationToken)
+    {
+        var inputText = await fileContentCache.GetAsync(request.TextDocument.Uri);
+
+        var loggerFactory = NullLoggerFactory.Instance;
+        var reader = new InterlisReader(loggerFactory);
+
+        // token stream from Lexer
+        var tokenStream = reader.RunLexer(new StringReader(inputText));
+
+        // parse tree from parser
+        var interlisParser = reader.GetParser(tokenStream);
+        var parseTree = interlisParser.interlis();
+
+        var formatter = new FormatterVisitor(loggerFactory, tokenStream);
+        var formattedOutput = formatter.VisitInterlis(parseTree);
+
+        var lastToken = tokenStream.GetTokens().Last();
+        var endPosition = new Position(lastToken.Line, lastToken.Column);
+
+        var edit = new TextEdit
+        {
+            Range = new Range(new Position(0, 0), endPosition),
+            NewText = formattedOutput.Content,
+        };
+
+        return new TextEditContainer(edit);
+    }
+}
